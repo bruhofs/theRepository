5b147f3f49281b2223dcded31aec45038822b7b6
bruhofs <brunohofstatter@ienh.com.br>
2023-03-13 19:34:03 -0300

    teste

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,10 @@
-print('funções')
+print('''       Funções
+Escolha sua função:
+    - Inverter string (1)
+    - Ordenar caracteres da string (2)
+    - Retornar dia da semana para data (3)
+    - Detectar palíndromos (4)
+    - Retornar número par ou ímpar (5)
+    - Sortear um número aleátorio com intervalo (6)
+    - Verificar se palavra ou caracter existe em string (7)
+    - Imprimir o número Pi com x casas decimais (8)''')
